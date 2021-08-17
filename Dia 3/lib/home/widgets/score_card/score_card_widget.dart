f72eae921b90d881b3280b1a78838cce6aaeaba2
Kelvin Rodrigues <krdg10@live.com>
2021-08-17 02:12:21 -0300

    Dia 3

---
+++ Dia 3/lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../core/core.dart';
+import '../chart/chart_widget.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  const ScoreCardWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Expanded(
+              flex: 1, 
+              child: ChartWidget()
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text("Vamos Começar", style: AppTextStyles.heading),
+                  Text("Complete os desafios e transforme em conhecimento", style: AppTextStyles.body,),
+                ],
+                ),
+              ),
+              )
+          ],
+          ),
+        )
+
+        
+      ),
+    );
+  }
+}
